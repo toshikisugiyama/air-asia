0fe0a595c2ebcd9ce036cf0790f75cc8c6252888
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-07-04 23:58:03 +0900

    サイドバーにコミュニケーションツール表示

--- wp-content/themes/apt-pc/sidebar-common.php
+++ wp-content/themes/apt-pc/sidebar-common.php
@@ -1,10 +1,11 @@
 
       <div class="rss_link">
-        <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
+        <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
       </div>
       <div class="textwidget">
         <p>
           <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/bnr_blog.jpg" class="" width="215" height="110" alt="社長ブログ" />ジャカルタで働く社長のブログ。</a>
         </p>
       </div>
-      <img src="<?php bloginfo('template_url'); ?>/images/sns.png" alt="" />
+      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=215&height=342&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="215" height="342" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+      <a class="twitter-timeline" href="https://twitter.com/llwz886q?ref_src=twsrc%5Etfw">Tweets by llwz886q</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
