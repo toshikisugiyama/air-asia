8f3dc7000ce3a28dd9a5f4488f5fe8d464407ba4
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-07-05 22:59:45 +0900

    営業所情報の専用管理画面作成

--- wp-content/themes/apt-pc/content-archive-branch.php
+++ wp-content/themes/apt-pc/content-archive-branch.php
@@ -1,14 +1,13 @@
-<div class="branch_info clearfix"></div>
+<div class="branch_info clearfix">
 <h2 class="page_ttl"><?php the_title(); ?></h2>
-<div class="branch_info">
-  <h2>営業所情報</h2>
+
   <table>
     <tr>
       <th>住所</th>
       <td>〒<?php echo esc_html($post->cf_zip); ?>
 <?php echo esc_html($post->cf_address); ?><br/>
         <a href="https://maps.google.co.jp/maps?q=<?php echo esc_attr($post->cf_lat); ?>,<?php echo esc_attr($post->cf_lng); ?>+(<?php echo urlencode(esc_attr($post->post_title));
-?>)&amp;iwlocA&amp;z=<?php echo esc_attr($post->cf_zoom); ?>" target="_blank">Googleマップで表示する</a>
+?>)&amp;iwloc=A&amp;z=<?php echo esc_attr($post->cf_zoom); ?>" target="_blank">Googleマップで表示する</a>
       </td>
     </tr>
     <tr>
