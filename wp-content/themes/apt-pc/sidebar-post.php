97a754ac3488a23bc97681db7f700f357cadeabd
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-07-07 14:29:46 +0900

    ツアー情報個別ページの表示

--- wp-content/themes/apt-pc/sidebar-post.php
+++ wp-content/themes/apt-pc/sidebar-post.php
@@ -6,7 +6,7 @@
         <h2>お知らせ</h2>
         <ul class="sub_navi">
 <?php wp_list_pages(array('include' => get_page_by_path('news')->ID, 'title_li' => 0)); ?>
-<?php wp_list_categories(array('title_li' => false, 'hide_empty' => ture, 'current_category' => $cat_id)); ?>
+<?php wp_list_categories(array('title_li' => false, 'hide_empty' => true, 'current_category' => $cat_id)); ?>
         </ul>
       </div>
 <?php get_template_part('sidebar-common'); ?>
