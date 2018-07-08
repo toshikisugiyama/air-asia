4af7c1f0053b0984e3e98afbbd1d999cb510ff6d
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-07-08 17:22:15 +0900

    トップページにツアー一覧を表示させる

--- wp-content/themes/apt-pc/front-page.php
+++ wp-content/themes/apt-pc/front-page.php
@@ -1,32 +1,22 @@
 <?php get_header(); ?>
   <div class="main_image">
-    <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/top/main_image.png" width="950" height="329" alt="想像以上の旅をご提供いたします。" /></a>
+    <a href="<?php echo get_permalink(get_page_by_path('tour-info')); ?>"><img src="<?php bloginfo('template_url'); ?>/images/top/main_image.png" width="950" height="329" alt="想像以上の旅をご提供いたします。" /></a>
   </div><!-- .main_image end -->
   <div id="container">
     <div id="main" role="main">
       <div id="content">
         <div id="tour_info" class="clearfix">
           <h2><img src="<?php bloginfo('template_url'); ?>/images/top/ttl_tour.png" width="183" height="25" alt="最新ツアー情報"></h2>
-          <p class="list_link"><a href="#"><img src="<?php bloginfo('template_url'); ?>/images/top/btn_linklist.png" width="80" height="29" alt="一覧を見る"></a></p>
+          <p class="list_link"><a href="<?php echo get_permalink(get_page_by_path('tour-info')); ?>"><img src="<?php bloginfo('template_url'); ?>/images/top/btn_linklist.png" width="80" height="29" alt="一覧を見る"></a></p>
           <div class="inner clearfix">
-            <div class="tour_column">
-              <h3><a href="#">ハワイラグジュアリーステイ3日間</a></h3>
-              <p class="tour_info_top">
-                <span class="tour_category hawaii-guam">ハワイ・グアム</span>
-                <span class="tour_date">設定期間：2014/6/16&#xFF5E;2014/9/30</span>
-              </p>
-              <p class="tour_image"><a href="#"><img width="208" height="138" src="<?php bloginfo('template_url'); ?>/images/top/t1main-208x138.jpg" alt="ハワイラグジュアリーステイ3日間" /></a></p>
-              <dl class="tour_info_foot clearfix">
-                <dt>旅行代金</dt>
-                <dd>120,000円&#xFF5E;216,000円</dd>
-                <dt>訪問都市</dt>
-                <dd>ホノルル（オアフ島）</dd>
-                <dt>出発地</dt>
-                <dd>成田, 名古屋, 札幌</dd>
-                <dt>シートクラス</dt>
-                <dd>ファーストクラス</dd>
-              </dl>
-            </div>
+<?php
+  query_posts(array('post_type'=>'tour','posts_per_page'=>6));
+  while (have_posts()) :
+    the_post();
+    get_template_part('content-top-tour');
+  endwhile;
+  wp_reset_query();
+?>
           </div><!-- end .inner -->
         </div><!-- end #tourInfo -->
         <div id="top_info" class="clearfix">
